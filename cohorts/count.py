421bcd1458368a03338a43f0d1dde8d376ea8c35
Tavi Nathanson <tavi.nathanson@gmail.com>
2016-03-23 19:39:47 -0400

    Add easy plotting for cohorts

---
+++ cohorts/count.py
@@ -0,0 +1,66 @@
+# Copyright (c) 2016. Mount Sinai School of Medicine
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import numpy as np
+import pandas as pd
+
+from varcode import EffectCollection
+from varcode.effects import Substitution
+
+def snv_count(cohort):
+    sample_variants = cohort.load_variants()
+    def count_func(sample):
+        if sample in sample_variants:
+            return len(sample_variants[sample])
+        return np.nan
+    return count(cohort, count_func, count_col="snv_count")
+
+def nonsynonymous_snv_count(cohort):
+    sample_nonsynonymous_effects = cohort.load_effects(only_nonsynonymous=True)
+    def count_func(sample):
+        if sample in sample_nonsynonymous_effects:
+            return len(sample_nonsynonymous_effects[sample])
+        return np.nan
+    return count(cohort, count_func, count_col="nonsynonymous_snv_count")
+
+def missense_snv_count(cohort):
+    sample_nonsynonymous_effects = cohort.load_effects(only_nonsynonymous=True)
+    sample_missense_effects = dict(
+        [(sample,
+          EffectCollection(
+              [effect for effect in effects if type(effect) == Substitution]))
+         for (sample, effects) in sample_nonsynonymous_effects.items()])
+    def count_func(sample):
+        if sample in sample_missense_effects:
+            return len(sample_missense_effects[sample])
+        return np.nan
+    return count(cohort, count_func, count_col="missense_snv_count")
+
+def neoantigen_count(cohort):
+    sample_neoantigens = cohort.load_neoantigens()
+    def count_func(sample):
+        if sample in sample_neoantigens["sample_id"].unique():
+            return len(sample_neoantigens[sample_neoantigens["sample_id"] == sample])
+        return np.nan
+    return count(cohort, count_func, count_col="neoantigen_count")
+
+def count(cohort, count_func, count_col):
+    df = cohort.clinical_dataframe.copy()
+    df[count_col] = df[cohort.clinical_dataframe_id_col].map(count_func)
+    original_len = len(df)
+    df = df[~df[count_col].isnull()]
+    updated_len = len(df)
+    if updated_len < original_len:
+        print("Missing count for %d samples: from %d to %d" % (original_len - updated_len, original_len, updated_len))
+    return count_col, df
---
+++ cohorts/survival.py
@@ -0,0 +1,118 @@
+# Copyright (c) 2016. Mount Sinai School of Medicine
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import print_function
+
+from lifelines import KaplanMeierFitter
+from lifelines.statistics import logrank_test
+
+def plot_kmf(df, 
+             condition_col, 
+             censor_col, 
+             survival_col, 
+             threshold=None,
+             title=None,
+             xlabel=None,
+             ax=None,
+             print_as_title=False):
+    """
+    Plot survival curves by splitting the dataset into two groups based on
+    condition_col
+
+    if threshold is defined, the groups are split based on being > or <
+    condition_col
+
+    if threshold == 'median', the threshold is set to the median of condition_col
+
+    Parameters
+    ----------
+        df: dataframe
+        condition_col: string, column which contains the condition to split on
+        survival_col: string, column which contains the survival time
+        censor_col: string,
+        threshold: int or string, if int, condition_col is thresholded,
+                                  if 'median', condition_col thresholded 
+                                  at its median
+        title: Title for the plot, default None
+        ax: an existing matplotlib ax, optional, default None
+        print_as_title: bool, optional, whether or not to print text
+          within the plot's title vs. stdout, default False
+    """
+    kmf = KaplanMeierFitter()
+    if threshold is not None:
+        if threshold == 'median':
+            threshold = df[condition_col].median()
+        condition = df[condition_col] > threshold
+        label = '{} > {}'.format(condition_col, threshold)
+    else:
+        condition = df[condition_col]
+        label = '{}'.format(condition_col)
+                       
+    df_with_condition = df[condition]
+    df_no_condition = df[~condition]
+    survival_no_condition = df_no_condition[survival_col]
+    survival_with_condition = df_with_condition[survival_col]
+
+    event_no_condition = (df_no_condition[censor_col].astype(bool))
+    event_with_condition = (df_with_condition[censor_col].astype(bool))
+                  
+    kmf.fit(survival_no_condition, event_no_condition, label="")
+    if ax:
+        kmf.plot(ax=ax, show_censors=True, ci_show=False)
+    else:
+        ax = kmf.plot(show_censors=True, ci_show=False)
+
+    kmf.fit(survival_with_condition, event_with_condition, label=(label))
+    kmf.plot(ax=ax, show_censors=True, ci_show=False)
+
+    no_cond_str = "# no condition {}".format(len(survival_no_condition))
+    cond_str = "# with condition {}".format(len(survival_with_condition))
+    if title:
+        ax.set_title(title)
+    elif print_as_title:
+        ax.set_title("%s | %s" % (no_cond_str, cond_str))
+    else:
+        print(no_cond_str)
+        print(cond_str)
+
+    if xlabel:
+        ax.set_xlabel(xlabel)
+ 
+    results = logrank_test(survival_no_condition, 
+                           survival_with_condition, 
+                           event_observed_A=event_no_condition, 
+                           event_observed_B=event_with_condition)
+    return results
+
+def logrank(df,
+            condition_col,
+            censor_col,
+            survival_col,
+            threshold=None):
+    if threshold is not None:
+        if threshold == 'median':
+            threshold = df[condition_col].median()
+        condition = df[condition_col] > threshold
+    else:
+        condition = df[condition_col]
+    df_with_condition = df[condition]
+    df_no_condition = df[~condition]
+    survival_no_condition = df_no_condition[survival_col]
+    survival_with_condition = df_with_condition[survival_col]
+    event_no_condition = (df_no_condition[censor_col].astype(bool))
+    event_with_condition = (df_with_condition[censor_col].astype(bool))
+    return logrank_test(survival_no_condition, 
+                        survival_with_condition, 
+                        event_observed_A=event_no_condition, 
+                        event_observed_B=event_with_condition)
