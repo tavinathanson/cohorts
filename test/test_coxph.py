31be89925d425048f6fcec46b3bdfe19bce599d1
Jacki Buros <jackinovik@gmail.com>
2016-06-07 19:29:25 +0000

    added / cleaned up test cases

--- test/test_coxph.py
+++ test/test_coxph.py
@@ -19,9 +19,10 @@ from cohorts.coxph import *
 import pandas as pd
 import numpy as np
 import re
+import statsmodels as sm
 from nose.tools import raises, eq_, ok_
 
-def prep_sample_coxph_data():
+def prep_tcga_coxph_data():
     d = pd.DataFrame(np.genfromtxt('nationwidechildrens.org_clinical_patient_blca.txt', delimiter='\t', skip_header = 1, names = True, dtype=None, comments="CDE_ID", autostrip=True))
     d['status'] = (d.vital_status == 'Dead').astype(int)
     def mk_int(s, conv = int):
@@ -35,12 +36,45 @@ def prep_sample_coxph_data():
     d['time'] = d.apply(lambda row: min(row.days_to_death, row.days_to_last_followup), axis = 1)
     return(d)
 
-def test_coxph_model():
-    d = prep_sample_coxph_data()
-    result = coxph_model(data = d,
+def prep_alt_coxph_data():
+    d = sm.datasets.get_rdataset('cancer','survival').data
+    d['status'] = (d.status == 2).astype(int)
+    d['ecog'] = d['ph.ecog']
+    d['karno'] = d['ph.karno']
+    d['weightloss'] = d['wt.loss']
+    return(d)
+
+def test_coxph_model_tcga():
+    d = prep_tcga_coxph_data()
+    cv, data, concordance  = coxph_model(data = d,
                          formula = '~ gender + height + weight + age_at_initial_pathologic_diagnosis',
                          time_col = 'time',
                          event_col = 'status',
                          )
-    return(result)
+    return(cv)
 
+def test_coxph_model_alt():
+    d = prep_alt_coxph_data()
+    cv, data, concordance = coxph_model(data = d,
+                                        formula = '~ age + sex + ecog + karno',
+                                        time_col = 'time',
+                                        event_col = 'status',
+                                        )
+    return(cv)
+
+## example with missing data for some covariates
+def test_coxph_model_alt_missing():
+    d = prep_alt_coxph_data()
+    cv, data, concordance = coxph_model(data = d,
+                                        formula = '~ age + sex + ecog + karno + weightloss',
+                                        time_col = 'time',
+                                        event_col = 'status',
+                                        )
+    return(cv)
+
+## test bootstrap code
+def test_coxph_bootstrap():
+    d = prep_alt_coxph_data()
+    models = {'first_model': '~ age + sex + ecog + karno', 'second_model': '~ age + sex + ecog'}
+    results = bootstrap_coxph(data = d, models = models, time_col = 'time', event_col = 'status')
+    return(results) 
