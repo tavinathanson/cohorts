bd5288f32a9d92ed83d9cba0c19594b9d0b17702
Eliza Chang <eliza.j.chang@gmail.com>
2016-08-05 12:12:55 -0700

    attempts to get R working on travis

--- cohorts/R_code/packages.R
+++
@@ -1,3 +0,0 @@
-# commands to install needed R packages
-
-install.packages('deconstructSigs', repos='http://cran.us.r-project.org', dependencies=TRUE)
---
+++ install_r_packages.R
@@ -0,0 +1,3 @@
+# commands to install needed R packages
+cran_mirror <- "http://cran.us.r-project.org"
+install.packages('deconstructSigs', repos=cran_mirror, dependencies=TRUE)
