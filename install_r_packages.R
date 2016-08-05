a1db4af0b4896856f350dd83fd8a3f8ff63e6192
Eliza Chang <eliza.j.chang@gmail.com>
2016-08-05 13:34:21 -0700

    explicitly added R package dependencies installs

--- install_r_packages.R
+++ install_r_packages.R
@@ -1,3 +1,17 @@
 # commands to install needed R packages
+
+bio_source <- "https://bioconductor.org/biocLite.R"
+source(bio_source)
+biocLite("BSgenome")
+biocLite("BSgenome.Hsapiens.UCSC.hg19")
+biocLite("GenomeInfoDb")
+biocLite("VariantAnnotation")
+
+install.packages('plyr')
+install.packages('Rcpp')
+install.packages('testthat')
+install.packages('knitr')
+install.packages('reshape2', dependencies=TRUE)
+
 cran_mirror <- "http://cran.us.r-project.org"
 install.packages('deconstructSigs', repos=cran_mirror, dependencies=TRUE)
