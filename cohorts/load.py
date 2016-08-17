38a6c3b7e3394e6fecc8ea678961228a15ff2141
Tavi Nathanson <tavi.nathanson@gmail.com>
2016-08-17 20:35:04 +0000

    Expand plot_joint to return correlation information

--- cohorts/load.py
+++ cohorts/load.py
@@ -43,7 +43,7 @@ from scipy.stats import pearsonr
 
 from .utils import strip_column_names as _strip_column_names
 from .survival import plot_kmf
-from .plot import mann_whitney_plot, fishers_exact_plot, roc_curve_plot
+from .plot import mann_whitney_plot, fishers_exact_plot, roc_curve_plot, stripboxplot, CorrelationResults
 from .collection import Collection
 from .varcode_utils import (filter_variants, filter_effects,
                             filter_neoantigens, filter_polyphen)
@@ -1220,8 +1220,8 @@ class Cohort(Collection):
             ci_show=ci_show)
         return results
 
-    def plot_joint(self, on, on_two=None, stat_func=pearsonr, **kwargs):
-        """Plot a jointplot.
+    def plot_joint(self, on, on_two=None, plot_type="jointplot", stat_func=pearsonr, **kwargs):
+        """Plot the correlation between two variables.
 
         Parameters
         ----------
@@ -1229,14 +1229,29 @@ class Cohort(Collection):
             See `cohort.load.as_dataframe`
         on_two : function, optional
             Can specify the second function here rather than creating a list.
+        plot_type : str, optional
+            Specify "jointplot" or "boxplot".
+        stat_func : function, optional.
+            Specify which function to use for the statistical test.
         """
+        if plot_type not in ["boxplot", "jointplot"]:
+            raise ValueError("Invalid plot_type %s" % plot_type)
         if on_two is not None:
             on = [on, on_two]
         plot_cols, df = self.as_dataframe(on, **kwargs)
         for plot_col in plot_cols:
             df = filter_not_null(df, plot_col)
-        p = sb.jointplot(data=df, x=plot_cols[0], y=plot_cols[1], stat_func=stat_func)
-        return p
+        x_col = plot_cols[0]
+        y_col = plot_cols[1]
+        series_x = df[x_col]
+        series_y = df[y_col]
+        coeff, p_value = stat_func(series_x, series_y)
+        if plot_type == "jointplot":
+            plot = sb.jointplot(data=df, x=x_col, y=y_col, stat_func=stat_func)
+        else:
+            plot = stripboxplot(data=df, x=x_col, y=y_col)
+        return CorrelationResults(coeff=coeff, p_value=p_value, stat_func=stat_func,
+                                  series_x=series_x, series_y=series_y, plot=plot)
 
     def _list_patient_ids(self):
         """ Utility function to return a list of patient ids in the Cohort
