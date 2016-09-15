f56e9e0c5d1451a19c7e09630cb890a0b7968280
Tavi Nathanson <tavi.nathanson@gmail.com>
2016-09-15 18:16:41 -0400

    Remove another load.py reference

--- cohorts/__init__.py
+++ cohorts/__init__.py
@@ -12,7 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .load import *
+from .cohort import Cohort
+from .patient import Patient
+from .sample import Sample
 
 from ._version import get_versions
 __version__ = get_versions()['version']
