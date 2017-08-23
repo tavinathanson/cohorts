6f39c6bcfa5ec8eb6ce4d7df698a8cd88f134f87
Jacki Buros Novik <jackinovik@gmail.com>
2017-08-23 16:15:31 -0400

    remove extraneous frameshift functions

--- cohorts/functions.py
+++ cohorts/functions.py
@@ -180,13 +180,6 @@ exonic_variant_count = count_effects_function_builder(
     filterable_effect_function=lambda filterable_effect: (
         isinstance(filterable_effect.effect, Exonic)))
 
-exonic_frameshift_variant_count = count_effects_function_builder(
-    "exonic_frameshift_variant_count",
-    only_nonsynonymous=False,
-    filterable_effect_function=lambda filterable_effect: (
-        isinstance(filterable_effect.effect, Exonic) and
-        isinstance(filterable_effect.effect, FrameShift)))
-
 exonic_snv_count = count_effects_function_builder(
     "exonic_snv_count",
     only_nonsynonymous=False,
@@ -251,13 +244,6 @@ exonic_frameshift_insertion_count = count_effects_function_builder(
         filterable_effect.variant.is_insertion and
         isinstance(filterable_effect.effect, FrameShift) and
         isinstance(filterable_effect.effect, Exonic)))
-
-exonic_frameshift_snv_count = count_effects_function_builder(
-    "exonic_frameshift_snv_count",
-    only_nonsynonymous=False,
-    filterable_effect_function=lambda filterable_effect: (
-        isinstance(filterable_effect.effect, Exonic) and
-        isinstance(filterable_effect.effect, FrameShift)))
 
 missense_snv_and_nonsynonymous_indel_count = count_effects_function_builder(
     "missense_snv_and_nonsynonymous_indel_count",
