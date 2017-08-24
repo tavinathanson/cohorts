0e9ca4557a734e0bcf4dcc0da36f1429ecdad9ce
Jacki Buros Novik <jackinovik@gmail.com>
2017-08-24 16:21:56 -0400

    make separate cohorts.cohort.caching logger; move _hash_filter_fn & related code to utils

--- cohorts/utils.py
+++ cohorts/utils.py
@@ -21,6 +21,8 @@ from os import path
 import copy
 import pickle
 import hashlib
+import dill
+import inspect
 
 logger = logging.getLogger(__name__)
 
@@ -218,8 +220,46 @@ def set_attributes(obj, additional_data):
             raise ValueError("Key %s in additional_data already exists in this object" % key)
         setattr(obj, _strip_column_name(key), value)
 
+def get_function_name(fn, default="None"):
+    """ Return name of function, using default value if function not defined
+    """
+    if fn is None:
+        fn_name = default
+    else:
+        fn_name = fn.__name__
+    return fn_name
+
 def md5_hash(o):
     return hashlib.md5(pickle.dumps(o)).hexdigest()
+
+def hash_function(fn, **kwargs):
+    """ Construct string representing state of a function `fn`
+        Used to cache filtered variants or effects uniquely depending on fn passed through kwargs or in closure
+    """
+    fn_name = get_function_name(fn, default="filter-none")
+    logger.debug("Computing hash for fn: {} with kwargs {}".format(fn_name, str(dict(**kwargs))))
+    # hash function source code
+    fn_source = str(dill.source.getsource(fn))
+    pickled_fn_source = pickle.dumps(fn_source) ## encode as byte string
+    hashed_fn_source = int(hashlib.sha1(pickled_fn_source).hexdigest(), 16) % (10 ** 11)
+    # hash kwarg values
+    kw_dict = dict(**kwargs)
+    kw_hash = list()
+    if not kw_dict:
+        kw_hash = "default"
+    else:
+        kw_hash = make_hash(kw_dict)
+    # hash closure vars - for case where fn is defined within closure of fn
+    closure = dict()
+    nonlocals = inspect.getclosurevars(fn).nonlocals
+    closure_hash = make_hash(nonlocals)
+    # construct final string comprising hashed components
+    fn_as_string = ".".join(["-".join([fn_name,
+                                    str(hashed_fn_source)]),
+                             kw_hash,
+                             closure_hash]
+                            )
+    return fn_as_string
 
 DictProxyType = type(object.__dict__)
 def make_hash(o):
@@ -245,11 +285,16 @@ def make_hash(o):
             if not k.startswith("__"):
                 o2[k] = v
         o = o2
+    if inspect.isfunction(o):
+        return hash_function(o)
     if isinstance(o, (set, tuple, list)):
         return tuple([make_hash(e) for e in o])
     elif not isinstance(o, dict):
         return md5_hash(o)
     new_o = copy.deepcopy(o)
-    for k, v in new_o.items():
-        new_o[k] = make_hash(v)
-    return md5_hash(tuple(frozenset(sorted(new_o.items()))))
+    if len(new_o) == 0:
+        return md5_hash('{}')
+    else:
+        for k, v in new_o.items():
+            new_o[k] = make_hash(v)
+        return md5_hash(tuple(frozenset(sorted(new_o.items()))))
