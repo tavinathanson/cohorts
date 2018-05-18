28441c1091ef1c6f5486d0fe7ffb2a4e260bab7a
Jacki Buros Novik <jackinovik@gmail.com>
2018-05-18 16:32:04 -0400

    add varscan2 and mutect2 vcf formats

--- cohorts/variant_stats.py
+++ cohorts/variant_stats.py
@@ -15,11 +15,52 @@
 # pylint: disable=unsubscriptable-object
 from collections import namedtuple
 import vcf
+import warnings
 
 VariantStats = namedtuple("VariantStats",
                           ["depth", "alt_depth", "variant_allele_frequency"])
 SomaticVariantStats = namedtuple("SomaticVariantStats",
                           ["tumor_stats", "normal_stats"])
+
+def varscan2_somatic_variant_stats(variant, variant_metadata):
+    """Parse out the variant calling statistics for a given variant from a VarScan2 VCF
+    
+    Parameters
+    ----------
+    variant : varcode.Variant
+    sample_info : dict
+        Dictionary of sample to variant calling statistics, corresponds to the sample columns
+        in a VarScan2 VCF
+
+    Returns
+    -------
+    SomaticVariantStats
+    """
+    sample_info = variant_metadata["sample_info"]
+    # Ensure there are exactly two samples in the VCF, a tumor and normal
+    assert len(sample_info) == 2, "More than two samples found in the somatic VCF"
+    tumor_stats = _varscan2_variant_stats(variant, sample_info["TUMOR"])
+    normal_stats = _varscan2_variant_stats(variant, sample_info["NORMAL"])
+    return SomaticVariantStats(tumor_stats=tumor_stats, normal_stats=normal_stats)
+
+def _varscan2_variant_stats(variant, sample_info):
+    """Parse a single sample"s variant calling statistics based on VarScan2 VCF output
+
+    Parameters
+    ----------
+    variant : varcode.Variant
+    sample_info : dict
+        Dictionary of VarScan2-specific variant calling fields
+
+    Returns
+    -------
+    VariantStats
+    """
+    depth = int(sample_info["RD"] + sample_info["AD"])
+    alt_depth = int(sample_info["AD"])
+    vaf = float(sample_info["FREQ"].replace("%", ""))/float(100)
+    return VariantStats(depth=depth, alt_depth=alt_depth, variant_allele_frequency=vaf)
+
 
 def strelka_somatic_variant_stats(variant, variant_metadata):
     """Parse out the variant calling statistics for a given variant from a Strelka VCF
@@ -35,7 +76,6 @@ def strelka_somatic_variant_stats(variant, variant_metadata):
     -------
     SomaticVariantStats
     """
-
     sample_info = variant_metadata["sample_info"]
     # Ensure there are exactly two samples in the VCF, a tumor and normal
     assert len(sample_info) == 2, "More than two samples found in the somatic VCF"
@@ -123,6 +163,8 @@ def _mutect_variant_stats(variant, sample_info):
     """
 
     # Parse out the AD (or allele depth field), which is an array of [REF_DEPTH, ALT_DEPTH]
+    if len(sample_info["AD"]) == 1:
+        import pdb; pdb.set_trace()
     ref_depth, alt_depth = sample_info["AD"]
     depth = int(ref_depth) + int(alt_depth)
     vaf = float(alt_depth) / depth
@@ -160,19 +202,34 @@ def maf_somatic_variant_stats(variant, variant_metadata):
         normal_stats = _maf_variant_stats(variant, variant_metadata, prefix="n")
     return SomaticVariantStats(tumor_stats=tumor_stats, normal_stats=normal_stats)
 
+def _vcf_reader(variant_file, *args, **kwargs):
+    try:
+        vcf_reader = vcf.Reader(open(variant_file), *args, **kwargs)
+    except UnicodeDecodeError:
+        # per https://github.com/jamescasbon/PyVCF/issues/217#issuecomment-263851708
+        vcf_reader = vcf.Reader(filename=variant_file, *args, **kwargs)
+    return vcf_reader
+
 def _vcf_is_strelka(variant_file, variant_metadata):
     """Return True if variant_file given is in strelka format
     """
     if "strelka" in variant_file.lower():
         return True
-    elif "NORMAL" in variant_metadata["sample_info"].keys():
-        return True
+    # is problematic -- too general!!
+    #elif "NORMAL" in variant_metadata["sample_info"].keys():
+    #    return True
     else:
-        vcf_reader = vcf.Reader(open(variant_file, "r"))
+        vcf_reader = _vcf_reader(variant_file, "r")
         try:
             vcf_type = vcf_reader.metadata["content"]
         except KeyError:
             vcf_type = ""
+        try:
+            vcf_type.lower()
+        except AttributeError:
+            # catching this here since strings are also lists
+            if isinstance(vcf_type, list) and "strelka" in vcf_type[0].lower():
+                return True
         if "strelka" in vcf_type.lower():
             return True
     return False
@@ -182,15 +239,35 @@ def _vcf_is_maf(variant_file):
     """
     return ".maf" in variant_file.lower()
 
+def _vcf_is_varscan2(variant_file, variant_metadata):
+    """Return True if variant_file given is in varscan2 format
+    """
+    if "varscan2" in variant_file.lower():
+        return True
+    elif "NORMAL" in variant_metadata['sample_info'].keys() and "DP4" in variant_metadata['sample_info']['NORMAL'].keys():
+        return True
+    return False
+
+def _vcf_is_mutect2(variant_file, variant_metadata):
+    """Return True is variant_file given is in mutect2 format
+    """
+    if "mutect2" in variant_file.lower():
+        return True
+    elif "TUMOR" in variant_metadata["sample_info"].keys() and "AD" in variant_metadata["sample_info"]["TUMOR"].keys():
+        return True
+    return False
+
 def _vcf_is_mutect(variant_file, variant_metadata):
     """Return True if variant_file give is in mutect format
     """
-    if "mutect" in variant_file.lower():
+    if "mutect" in variant_file.lower() and "mutect2" not in variant_file.lower():
         return True
     elif "GT" in variant_metadata["sample_info"].keys():
         return True
+    elif "TUMOR" in variant_metadata["sample_info"].keys() and "GT" in variant_metadata["sample_info"]["TUMOR"].keys():
+        return True
     else:
-        vcf_reader = vcf.Reader(open(variant_file, "r"))
+        vcf_reader = _vcf_reader(variant_file, "r")
         try:
             vcf_type = vcf_reader.metadata["GATKCommandLine"][0]["ID"]
         except KeyError:
@@ -230,7 +307,14 @@ def variant_stats_from_variant(variant,
         elif _vcf_is_mutect(variant_file=variant_file,
                             variant_metadata=variant_metadata):
             stats = mutect_somatic_variant_stats(variant, variant_metadata)
+        elif _vcf_is_mutect2(variant_file=variant_file,
+                             variant_metadata=variant_metadata):
+            stats = mutect_somatic_variant_stats(variant, variant_metadata)
+        elif _vcf_is_varscan2(variant_file=variant_file,
+                              variant_metadata=variant_metadata):
+            stats = varscan2_somatic_variant_stats(variant, variant_metadata)
         else:
+            import pdb; pdb.set_trace()
             raise ValueError("Cannot parse sample fields, variant file {} is from an unsupported caller.".format(variant_file))
         all_stats.append(stats)
     return merge_fn(all_stats)
